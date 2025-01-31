307928daa882b91d1d10712d1f51bea31d95eeb9
JoshFdo25 <cb011604@students.apiit.lk>
2025-01-31 21:00:30 +0530

    Update ProductController to transform image URLs for products

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -11,12 +11,20 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::with('category')->get();
+
+        $products->transform(function ($product) {
+            $product->image = url('storage/' . $product->image);
+            return $product;
+        });
+    
         return response()->json($products);
     }
 
     public function show($id)
     {
         $product = Product::with('category')->findOrFail($id);
+        $product->image = url('storage/' . $product->image);
+    
         return response()->json($product);
     }
 }
