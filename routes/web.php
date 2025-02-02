028314520589958e067a4d76c65df0008bd8a580
JoshFdo25 <cb011604@students.apiit.lk>
2025-02-02 13:30:53 +0530

    Fix duplicate home route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
